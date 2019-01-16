7bd1cd5f7cc50466ccbc20c42f14e381ebca9e1f
DianaLiz11 <dianamunoz86@live.com.mx>
2019-01-16 00:06:42 -0600

    Se agrega la funcion de Cifrar, asi como la estructura HTML de la pagina para Cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,25 @@
 window.cipher = {
-  // ... 
+  // ...
+  encode:(cadena,offset) =>{
+    let cadenaCodificada="";
+    for(let i=0; i< cadena.length; i++){
+      let letraAscii = cadena.toUpperCase().charCodeAt(i);
+      let nuevaLetraAscii = (letraAscii - 65 + parseInt(offset)) % 26 + 65;
+      let letraEsp = String.fromCharCode(nuevaLetraAscii);
+      cadenaCodificada = cadenaCodificada.concat(letraEsp);
+    }
+    //alert("El valor de la cadena es: "+cadenaCodificada);
+    return cadenaCodificada;
+  },
+
+  decode:(codigo,offsetD)=>{
+    let cadenaDecodificada="";
+    for(let i=0; i< codigo.length; i++){
+      let letraAsciiC = codigo.toUpperCase().charCodeAt(i);
+      let nuevaLetraAsciiC = (letraAsciiC - 65 - offsetD) % 26 + 65;
+      let letraEspC = String.fromCharCode(nuevaLetraAsciiC);
+      cadenaDecodificada = cadenaDecodificada.concat(letraEspC);
+    }
+    return cadenaDecodificada;
+  }
 };
---
+++ src/index.js
@@ -0,0 +1,18 @@
+//const botonCifrar = document.getElementById('btnCifrar');
+
+document.getElementById('btnCifrar').addEventListener('click',() => {
+  const cadenaParaCifrar = document.getElementById('txtCadena').value;
+  const desplazamiento = document.getElementById('txtOffset').value;
+  const textoCodificado = window.cipher.encode(cadenaParaCifrar,desplazamiento);
+  document.getElementById('pCodigo').innerHTML = `El texto codificado es: ${textoCodificado}`;
+});
+
+document.getElementById('btnCambiarACifrar').addEventListener('click',() => {
+  document.getElementById('paginaPrincipal').style.display = 'none';
+  document.getElementById('paginaCifrado').style.display = 'block';
+});
+
+document.getElementById('btnRegresar').addEventListener('click',() => {
+  document.getElementById('paginaPrincipal').style.display = 'block';
+  document.getElementById('paginaCifrado').style.display = 'none';
+});
